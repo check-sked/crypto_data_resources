6d106c84842471d420b811b8128eaa10263c2e16
Check <check.sked@gmail.com>
2023-01-09 18:00:42 -0500

    Finished script for stablecoin ID, symbol, and peg-type

--- defi_llama/stablecoin_id_peg_type.py
+++ defi_llama/stablecoin_id_peg_type.py
@@ -1,2 +1,23 @@
-# Use this endpoint https://stablecoins.llama.fi/stablecoins?includePrices=true to get symbol, ID, and peg type of all stables
-# This will be used to get IDs of coins to be fed into other API requests
+import requests
+import csv
+
+# Send a GET request to the endpoint
+response = requests.get("https://stablecoins.llama.fi/stablecoins?includePrices=true")
+
+# Check the status code to make sure the request was successful
+if response.status_code == 200:
+  # The request was successful, so parse the JSON response
+  data = response.json()
+  # Open a file for writing the data
+  with open('stablecoins.csv', 'w', newline='') as csvfile:
+    # Create a CSV writer
+    writer = csv.writer(csvfile)
+    # Write the header row
+    writer.writerow(['Symbol', 'ID', 'Peg Type'])
+    # Loop through the list of stablecoins
+    for stablecoin in data['peggedAssets']:
+      # Write the stablecoin data to the CSV file
+      writer.writerow([stablecoin['symbol'], stablecoin['id'], stablecoin['pegType']])
+else:
+  # The request was not successful, so print an error message
+  print("An error occurred while retrieving the data")
