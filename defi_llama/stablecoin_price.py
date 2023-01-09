9057fd010f3a497beba3348fca417d3a2b5d5384
Check <check.sked@gmail.com>
2023-01-09 18:50:53 -0500

    finished stablecoin prices script

--- defi_llama/stablecoin_price.py
+++ defi_llama/stablecoin_price.py
@@ -1,1 +1,48 @@
-# Use this endpoint https://stablecoins.llama.fi/stablecoinprices to get historical value of all stablecoins
+import requests
+import csv
+import datetime
+
+# Make a request to the stablecoin prices endpoint
+response = requests.get("https://stablecoins.llama.fi/stablecoinprices")
+
+# Check that the request was successful
+if response.status_code == 200:
+    # The request was successful, so parse the data
+    data = response.json()
+    
+    # Extract the list of prices
+    prices = data
+    
+    # Get the list of stablecoins
+    stablecoins = list(prices[0]["prices"].keys())
+
+    # Initialize a dictionary to store the previous day's prices for each stablecoin
+prev_prices = {}
+
+# Open a CSV file for writing
+with open('stablecoin_prices.csv', 'w', newline='') as csvfile:
+    # Create a CSV writer
+    writer = csv.writer(csvfile)
+    
+    # Write the header row
+    writer.writerow(["Date"] + stablecoins)
+    
+    # Write a row for each date
+    for date_prices in prices:
+        # Convert the Unix timestamp date to a human-readable date
+        date = datetime.datetime.fromtimestamp(date_prices['date']).strftime('%Y-%m-%d')
+        
+        # Build a list of prices for this date
+        row = [date]
+        for stablecoin in stablecoins:
+            if stablecoin in date_prices['prices']:
+                price = date_prices['prices'][stablecoin]
+            else:
+                # Use the previous day's price as the placeholder value
+                price = prev_prices.get(stablecoin, 0)  # Use null as the default value if the stablecoin has no previous price
+            row.append(price)
+            prev_prices[stablecoin] = price
+        writer.writerow(row)
+    else:
+    # There was an error with the request
+        print(f"Request failed with status code {response.status_code}")
