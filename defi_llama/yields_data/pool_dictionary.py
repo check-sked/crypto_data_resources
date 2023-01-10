bcfea7184566c340cfdf8c572da0a8f2d8fbdbbc
Check <check.sked@gmail.com>
2023-01-10 13:41:18 -0500

    Revised pools glossary to sort chain and project in alphabetical order

--- defi_llama/yields_data/pool_dictionary.py
+++
@@ -1,3 +0,0 @@
-# Endpoint https://yields.llama.fi/pools
-# GET chain, project, symbol, and pool
-# Put in csv
---
+++ defi_llama/yields_data/pools_glossary.py
@@ -0,0 +1,19 @@
+import csv
+import requests
+
+response = requests.get('https://yields.llama.fi/pools')
+data = response.json()
+
+data = sorted(data["data"], key=lambda x: (x["chain"], x["project"]))
+
+with open('pools_glossary.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Chain', 'Project', 'Symbol', 'Pool'])
+    for item in data:
+        project = item['project']
+        symbol = item['symbol']
+        pool = item['pool']
+        chain = item['chain']
+        writer.writerow([chain, project, symbol, pool])
+
+print("Data written to pools_glossary.csv")
