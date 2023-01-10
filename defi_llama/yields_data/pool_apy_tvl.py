1d10459da7e5340ed327ee8b9cf5a9933e098ed3
Check <check.sked@gmail.com>
2023-01-10 09:38:20 -0500

    Started gathering pool data

---
+++ defi_llama/yields_data/pool_apy_tvl.py
@@ -0,0 +1,4 @@
+# Endpoint https://yields.llama.fi/chart/747c1d2a-c668-4682-b9f9-296708a3dd90
+# Make ID dynamic input
+# Notice null values in JSON, write script to make null prev day values
+# Put in CSV
---
+++ defi_llama/yields_data/pool_dictionary.py
@@ -0,0 +1,3 @@
+# Endpoint https://yields.llama.fi/pools
+# GET chain, project, symbol, and pool
+# Put in csv
