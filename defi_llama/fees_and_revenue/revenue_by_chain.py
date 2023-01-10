845a1164fdc23e0a26272937780e66611fd1a753
Check <check.sked@gmail.com>
2023-01-10 15:02:10 -0500

    Created Fee and Revenue folder and added Python files

---
+++ defi_llama/fees_and_revenue/revenue_by_chain.py
@@ -0,0 +1,1 @@
+# Endpoint: https://api.llama.fi/overview/fees/ethereum?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=true&dataType=dailyRevenue
---
+++ defi_llama/fees_and_revenue/revenue_by_protocol.py
@@ -0,0 +1,7 @@
+# Endpoint https://api.llama.fi/summary/fees/{Protocol}?dataType={Time_Frame}Revenue
+
+# Make it sort by chain on a daily basis and have a sum column to total all
+
+# Protocol = "AAVE"
+
+# Time_Frame = "daily"
