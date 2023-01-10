f8a5604bcdd88660bd0218fc10b6487c58697a27
Check <check.sked@gmail.com>
2023-01-10 14:15:16 -0500

    Reformatted date column and added dynamic input for Pool ID

--- defi_llama/yields_data/pool_apy_tvl.py
+++ defi_llama/yields_data/pool_apy_tvl.py
@@ -1,7 +1,11 @@
 import csv
 import requests
+import datetime
 
-response = requests.get("https://yields.llama.fi/chart/747c1d2a-c668-4682-b9f9-296708a3dd90")
+# Input Pool ID (Pool IDs can be found in the Pool Glossary)
+Pool_ID = "747c1d2a-c668-4682-b9f9-296708a3dd90"
+
+response = requests.get(f"https://yields.llama.fi/chart/{Pool_ID}")
 data = response.json()
 
 # Open the file in write mode
@@ -14,7 +18,7 @@ with open('pool_tvl_apy.csv', mode='w', newline='') as file:
 
     # Write the data to the CSV file
     for item in data["data"]:
-        timestamp = item['timestamp']
+        timestamp = datetime.datetime.strptime(item['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%m/%d/%Y')
         tvlUSD = item['tvlUsd']
         apy = round(item['apy']/100, 5)
         writer.writerow([timestamp, tvlUSD, apy])
