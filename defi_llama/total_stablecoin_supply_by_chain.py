4952e9a3fd3231313e87f60f2bf2030b58877949
Check <check.sked@gmail.com>
2023-01-09 19:12:08 -0500

    stabelcoin supply by chain script started

--- defi_llama/total_stablecoin_supply_by_chain.py
+++ defi_llama/total_stablecoin_supply_by_chain.py
@@ -1,2 +1,35 @@
-# Use this endpoint https://stablecoins.llama.fi/stablecoincharts/Ethereum to get historical mcap of stablecoins on a specific chain
-# Use custom input for chain
+import requests
+import csv
+import datetime
+
+# Make a GET request to the endpoint
+response = requests.get('https://stablecoins.llama.fi/stablecoincharts/Ethereum')
+
+# Check the status code of the response
+if response.status_code == 200:
+    # Parse the response data
+    data = response.json()
+
+    # Open a CSV file for writing
+    with open('stablecoin_supply.csv', 'w', newline='') as csv_file:
+        # Create a CSV writer object
+        writer = csv.writer(csv_file)
+
+        # Write the header row
+        writer.writerow(['date', 'total_circulating_usd', 'total_minted_usd', 'total_bridged_to_usd'])
+
+        # Loop through the elements of the data list
+        for element in data:
+            # Get the data from the element
+            date = element['date']
+            date = datetime.datetime.fromtimestamp(int(date)).strftime('%Y-%m-%d')
+            stablecoin_supply = element['totalCirculating']
+            total_circulating_usd = element['totalCirculatingUSD']['peggedUSD']
+            total_minted_usd = element['totalMintedUSD']['peggedUSD']
+            total_bridged_to_usd = element['totalBridgedToUSD']['peggedUSD']
+
+            # Write the data to the CSV file
+            writer.writerow([date, total_circulating_usd, total_minted_usd, total_bridged_to_usd])
+else:
+    # Print an error message
+    print('Failed to get stablecoin supply data')
