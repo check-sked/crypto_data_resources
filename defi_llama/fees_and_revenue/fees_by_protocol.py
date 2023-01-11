407755f0f2377ac4554578a41552fb62f07462d3
Check <check.sked@gmail.com>
2023-01-11 18:35:25 -0500

    Finished fees_by_protocol.py with dynamic input for protocol and fee type

--- defi_llama/fees_and_revenue/fees_by_protocol.py
+++ defi_llama/fees_and_revenue/fees_by_protocol.py
@@ -1,7 +1,25 @@
-# Endpoint https://api.llama.fi/summary/fees/{Protocol}?dataType={Time_Frame}Fees
+import csv
+import requests
+from datetime import datetime
 
-# Make it sort by chain on a daily basis and have a sum column to total all
+# Input protocol to observe. Reference protocols_list_fees.csv for available protocols.
+Protocol = "Uniswap"
 
-# Protocol = "AAVE"
+# Input time period (daily or total)
+Type = "daily"
 
-# Time_Frame = "daily"
+response = requests.get(f"https://api.llama.fi/summary/fees/{Protocol}?dataType={Type}Fees")
+data = response.json()
+
+with open('fees_by_protocol.csv', mode='w', newline='') as csv_file:
+    fieldnames = ['date', 'fees']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    print("data written to fees_by_protocol.csv")
+
+    for day in data['totalDataChart']:
+        timestamp = day[0]
+        fees = day[1]
+        date = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
+        writer.writerow({'date': date, 'fees': fees})
+
