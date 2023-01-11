488a399a0b45291ee17aa5a40f4a21de14c05138
Check <check.sked@gmail.com>
2023-01-11 18:24:53 -0500

    Finished protocols_list_fees.py and added CSV

--- defi_llama/fees_and_revenue/protocols_list_fees.py
+++ defi_llama/fees_and_revenue/protocols_list_fees.py
@@ -1,1 +1,16 @@
-# https://api.llama.fi/overview/fees?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=true&dataType=dailyFees
+import csv
+import requests
+
+url = 'https://api.llama.fi/overview/fees?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=true&dataType=dailyFees'
+
+response = requests.get(url)
+data = response.json()
+
+with open('protocols_list_fees.csv', mode='w', newline='') as csv_file:
+    fieldnames = ['name', 'category']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    print("data written to protocols_list_fees.csv")
+
+    for protocol in data['protocols']:
+        writer.writerow({'name': protocol['name'], 'category': protocol['category']})
