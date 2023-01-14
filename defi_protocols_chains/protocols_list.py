1b62f9e72de39ae936b43a7515d32794d95a4721
Check <check.sked@gmail.com>
2023-01-14 11:44:51 -0500

    Added categories to protocols_list.py and error handling

--- defi_protocols_chains/protocols_list.py
+++ defi_protocols_chains/protocols_list.py
@@ -1,20 +1,34 @@
 import csv
 import requests
-from operator import itemgetter
 
-response = requests.get("https://api.llama.fi/protocols")
-data = response.json()
+try:
+    # Retrieve data from API
+    response = requests.get("https://api.llama.fi/protocols")
+    response.raise_for_status()
+    data = response.json()
 
-# Sort data alphabetically by name and by chain within name
-data = sorted(data, key=lambda x: x['name'])
-for protocol in data:
-    protocol['chains'] = sorted(protocol['chains'])
-
-with open('protocols_list.csv', mode='w') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Name", "Chain"])
-    print("data written to protocols_list.csv")
-
+    # Sort data alphabetically by name and by chain within name
+    data = sorted(data, key=lambda x: x['name'])
     for protocol in data:
-        for chain in protocol["chains"]:
-            writer.writerow([protocol["name"], chain])
+        protocol['chains'] = sorted(protocol['chains'])
+
+    with open('protocols_list.csv', mode='w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Name", "Chain","Category"])
+        print("data written to protocols_list.csv")
+
+        for protocol in data:
+            for chain in protocol["chains"]:
+                try:
+                    writer.writerow([protocol["name"], chain, protocol["category"]])
+                except KeyError:
+                    writer.writerow([protocol["name"], chain, ""])
+
+except requests.exceptions.HTTPError as errh:
+    print ("HTTP Error:",errh)
+except requests.exceptions.ConnectionError as errc:
+    print ("Error Connecting:",errc)
+except requests.exceptions.Timeout as errt:
+    print ("Timeout Error:",errt)
+except requests.exceptions.RequestException as err:
+    print ("Something went wrong:",err)
