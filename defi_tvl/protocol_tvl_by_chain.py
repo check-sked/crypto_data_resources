1551cc14f3a0e55e015504b9d6c197853828caa0
Check <check.sked@gmail.com>
2023-01-14 12:58:20 -0500

    Added protocol TVL by chain data

---
+++ defi_tvl/protocol_tvl_by_chain.py
@@ -0,0 +1,53 @@
+import csv
+import requests
+import datetime
+
+# Input protocol to observe
+Protocol ="aave"
+
+try:
+    # Retrieve data from API
+    response = requests.get(f"https://api.llama.fi/protocol/{Protocol}")
+    response.raise_for_status()
+    data = response.json()
+except requests.exceptions.HTTPError as errh:
+    print ("HTTP Error:",errh)
+except requests.exceptions.ConnectionError as errc:
+    print ("Error Connecting:",errc)
+except requests.exceptions.Timeout as errt:
+    print ("Timeout Error:",errt)
+except requests.exceptions.RequestException as err:
+    print ("Something went wrong:",err)
+
+
+# Create a list to hold the header row
+header_row = ['Date']
+
+# Create a list to hold the rows of data
+data_rows = []
+
+# Iterate through the chainTvls data
+for chain, tvl_data in data['chainTvls'].items():
+    if 'borrowed' in chain or 'staking' in chain or chain == 'pool2':
+        continue
+    header_row.append(chain)
+    for tvl in tvl_data['tvl']:
+        date = datetime.datetime.fromtimestamp(tvl['date']/1000).strftime('%Y-%m-%d %H:%M:%S')
+        found = False
+        for row in data_rows:
+            if row[0] == date:
+                row.append(tvl['totalLiquidityUSD'])
+                found = True
+                break
+        if not found:
+            data_rows.append([date, tvl['totalLiquidityUSD']])
+
+# Sort the data by date
+data_rows = sorted(data_rows, key=lambda x: x[0])
+
+# Write the data to a CSV file
+with open('tvl_by_protocol_chain.csv', mode='w') as file:
+    writer = csv.writer(file)
+    writer.writerow(header_row)
+    for row in data_rows:
+        writer.writerow(row)
