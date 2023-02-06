a1166aeea8997f7fe3663f426c7f0aab6aa93572
Check <check.sked@gmail.com>
2023-02-06 18:58:08 -0500

    Added staking by selected protocol data

---
+++ staking/staking_by_protocol.py
@@ -0,0 +1,36 @@
+import csv
+import datetime
+import requests
+
+# Set the API end point URL
+protocol = input("Enter a protocol: ")
+
+# Make the API request
+response = requests.get(f"https://api.llama.fi/protocol/{protocol}")
+
+# Check for successful request
+if response.status_code == 200:
+  # Get the data from the response
+  data = response.json()
+
+  # Get the TVL data
+  staking_data = data["chainTvls"]["staking"]["tvl"]
+
+  # Open a CSV file for writing
+  with open(f"{protocol}_staking.csv", "w", newline="") as csvfile:
+    # Create a CSV writer
+    writer = csv.writer(csvfile)
+
+    # Write the header row to the CSV file
+    writer.writerow(["Date", "Staking"])
+    print(f"Data written to {protocol}_staking.csv!")
+
+    # Write the TVL data to the CSV file
+    for datapoint in staking_data:
+      # Convert the date value to a human-readable string
+      date = datetime.datetime.fromtimestamp(int(datapoint["date"])).strftime('%m/%d/%Y')
+      tvl = datapoint["totalLiquidityUSD"]
+      writer.writerow([date, tvl])
+else:
+  # Print an error message
+  print("Failed to retrieve data")
