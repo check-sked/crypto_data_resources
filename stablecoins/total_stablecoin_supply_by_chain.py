0381c3ebaf4a002235b1536bcb35a4e5c249232b
Check <check.sked@gmail.com>
2023-06-24 12:09:44 -0400

    Updated to if __name__ == '__main__', updated chain input, and updated CSV file name

--- stablecoins/total_stablecoin_supply_by_chain.py
+++ stablecoins/total_stablecoin_supply_by_chain.py
@@ -2,47 +2,56 @@ import requests
 import csv
 import datetime
 
-# Input desired chain
-chain = "tron"
 
-# Make a GET request to the endpoint
-response = requests.get(f"https://stablecoins.llama.fi/stablecoincharts/{chain}")
+def save_stablecoin_supply_csv(chain):
+    # Make a GET request to the endpoint
+    response = requests.get(f"https://stablecoins.llama.fi/stablecoincharts/{chain}")
 
-# Check the status code of the response
-if response.status_code == 200:
-    # Parse the response data
-    data = response.json()
+    # Check the status code of the response
+    if response.status_code == 200:
+        # Parse the response data
+        data = response.json()
 
-    # Open a CSV file for writing
-    with open("stablecoin_supply.csv", "w", newline="") as csv_file:
-        # Create a CSV writer object
-        writer = csv.writer(csv_file)
+        # Open a CSV file for writing
+        with open(f"{chain}_stablecoin_supply.csv", "w", newline="") as csv_file:
+            # Create a CSV writer object
+            writer = csv.writer(csv_file)
 
-        # Write the header row
-        writer.writerow(
-            [
-                "date",
-                "total_circulating_usd",
-                "total_minted_usd",
-                "total_bridged_to_usd",
-            ]
-        )
-        print("data written to stablecoin_supply.csv")
-
-        # Loop through the elements of the data list
-        for element in data:
-            # Get the data from the element
-            date = element["date"]
-            date = datetime.datetime.fromtimestamp(int(date)).strftime("%Y-%m-%d")
-            stablecoin_supply = element["totalCirculating"]
-            total_circulating_usd = element["totalCirculatingUSD"]["peggedUSD"]
-            total_minted_usd = element["totalMintedUSD"]["peggedUSD"]
-            total_bridged_to_usd = element["totalBridgedToUSD"]["peggedUSD"]
-
-            # Write the data to the CSV file
+            # Write the header row
             writer.writerow(
-                [date, total_circulating_usd, total_minted_usd, total_bridged_to_usd]
+                [
+                    "date",
+                    "total_circulating_usd",
+                    "total_minted_usd",
+                    "total_bridged_to_usd",
+                ]
             )
-else:
-    # Print an error message
-    print("Failed to get stablecoin supply data")
+            print(f"Data written to {chain}_stablecoin_supply.csv")
+
+            # Loop through the elements of the data list
+            for element in data:
+                # Get the data from the element
+                date = element["date"]
+                date = datetime.datetime.fromtimestamp(int(date)).strftime("%Y-%m-%d")
+                stablecoin_supply = element["totalCirculating"]
+                total_circulating_usd = element["totalCirculatingUSD"]["peggedUSD"]
+                total_minted_usd = element["totalMintedUSD"]["peggedUSD"]
+                total_bridged_to_usd = element["totalBridgedToUSD"]["peggedUSD"]
+
+                # Write the data to the CSV file
+                writer.writerow(
+                    [
+                        date,
+                        total_circulating_usd,
+                        total_minted_usd,
+                        total_bridged_to_usd,
+                    ]
+                )
+    else:
+        # Print an error message
+        print("Failed to get stablecoin supply data")
+
+
+if __name__ == "__main__":
+    chain = input("Enter the chain: ")
+    save_stablecoin_supply_csv(chain)
