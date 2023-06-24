ff285d85be82818a72fc193fe1718bf8190bd8f8
Check <check.sked@gmail.com>
2023-06-24 12:06:49 -0400

    Updated to if __name__ == '__main__' format

--- stablecoins/stablecoin_supply_total.py
+++ stablecoins/stablecoin_supply_total.py
@@ -2,32 +2,40 @@ import csv
 import requests
 import datetime
 
-# Send GET request to the endpoint
-response = requests.get("https://stablecoins.llama.fi/stablecoincharts/all")
 
-# Check status code to make sure the request was successful
-if response.status_code != 200:
-    print("Failed to retrieve data. Status code:", response.status_code)
-    exit()
+def save_total_stablecoins_all_chains_csv():
+    # Send GET request to the endpoint
+    response = requests.get("https://stablecoins.llama.fi/stablecoincharts/all")
 
-# Extract data from the response
-data = response.json()
+    # Check status code to make sure the request was successful
+    if response.status_code != 200:
+        print("Failed to retrieve data. Status code:", response.status_code)
+        return
 
-# Open a CSV file for writing
-with open("total_stablecoins_all_chains.csv", "w", newline="") as csvfile:
-    # Create a CSV writer object
-    writer = csv.writer(csvfile)
+    # Extract data from the response
+    data = response.json()
 
-    # Write the header row
-    writer.writerow(["Date", "peggedUSD", "peggedEUR"])
+    # Open a CSV file for writing
+    with open("total_stablecoins_all_chains.csv", "w", newline="") as csvfile:
+        # Create a CSV writer object
+        writer = csv.writer(csvfile)
 
-    # Write the data rows
-    for item in data:
-        timestamp = int(item["date"])  # convert timestamp to integer
-        date = datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d")  # format timestamp as human-readable date
-        peggedUSD = item["totalCirculating"]["peggedUSD"]
-        peggedEUR = item["totalCirculating"]["peggedEUR"]
+        # Write the header row
+        writer.writerow(["Date", "peggedUSD", "peggedEUR"])
 
-        writer.writerow([date, peggedUSD, peggedEUR])
+        # Write the data rows
+        for item in data:
+            timestamp = int(item["date"])  # convert timestamp to integer
+            date = datetime.datetime.fromtimestamp(timestamp).strftime(
+                "%Y-%m-%d"
+            )  # format timestamp as human-readable date
+            peggedUSD = item["totalCirculating"]["peggedUSD"]
+            peggedEUR = item["totalCirculating"]["peggedEUR"]
 
-print("Data written to total_stablecoins_all_chains.csv")
+            writer.writerow([date, peggedUSD, peggedEUR])
+
+    print("Data written to total_stablecoins_all_chains.csv")
+
+
+if __name__ == "__main__":
+    save_total_stablecoins_all_chains_csv()
