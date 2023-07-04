73a8397657d5d8f8d8f7c60e7ac6498cb10c25d6
Check <check.sked@gmail.com>
2023-07-04 13:29:55 -0400

    Updated to if __name__ == __main__ format

--- bitcoin/all_blockchain_com_data.py
+++ bitcoin/all_blockchain_com_data.py
@@ -1,48 +1,41 @@
-import csv # For csv file creation
-import requests # For get request
-import datetime  # For date reformat
+import csv
+import requests
+import datetime
 
-# Name of chart that contains data
-chart_name = "estimated-transaction-volume"
 
-# Desired timespan; leave blank for default 1year (1month, 3month, 6month, 1year, 3years, all)
-timespan = "3years"
+def main():
+    chart_name = "estimated-transaction-volume"
+    timespan = "3years"
 
-try:
-    # Make a request to the endpoint
-    response = requests.get(f"https://api.blockchain.info/charts/{chart_name}?timespan={timespan}")
+    try:
+        response = requests.get(
+            f"https://api.blockchain.info/charts/{chart_name}?timespan={timespan}"
+        )
+        data = response.json()
+    except requests.exceptions.RequestException as e:
+        print("Error: Could not connect to endpoint")
+        print("Error details:", e)
+        exit()
+    except ValueError as e:
+        print("Error: Could not parse JSON response")
+        print("Error details:", e)
+        exit()
 
-    # Parse the JSON response
-    data = response.json()
+    points = data["values"]
 
-except requests.exceptions.RequestException as e:
-    print("Error: Could not connect to endpoint")
-    print("Error details:", e)
-    exit()
-except ValueError as e:
-    print("Error: Could not parse JSON response")
-    print("Error details:", e)
-    exit()
-
-# Extract the data points from the response
-points = data["values"]
-
-# Open a file for writing
-try:
     with open("blockchain.com.csv", "w", newline="") as csvfile:
-        # Create a CSV writer
         writer = csv.writer(csvfile)
 
-        # Write the header row
         writer.writerow(["date", chart_name])
 
-        # Write the data rows
         for point in points:
-            # Convert unix timestamp to formatted date string
-            date_string = datetime.datetime.fromtimestamp(point["x"]).strftime("%Y-%m-%d")
+            date_string = datetime.datetime.fromtimestamp(point["x"]).strftime(
+                "%Y-%m-%d"
+            )
             writer.writerow([date_string, point["y"]])
-except:
-    print("Error: Could not write to file")
-    exit()
 
-print("Data written to blockchain.com.csv")
+    print("Data written to blockchain.com.csv")
+
+
+if __name__ == "__main__":
+    main()
