335e17abc03615588ea99a4ae1d0b06f0f662381
Check <check.sked@gmail.com>
2023-07-24 09:09:59 -0400

    Updated to if __name__ == __main__ and cleaned up formatting + CSV file name

--- defi_fees_and_revenue/protocol_fees_by_chain.py
+++ defi_fees_and_revenue/protocol_fees_by_chain.py
@@ -1,35 +1,44 @@
 import requests
 import pandas as pd
 
-# Input chain to observe
-Chain = "Ethereum"
 
-#Time frame to observe (daily or weekly)
-Time = "daily"
+def main():
+    # Input chain to observe
+    Chain = input("Enter a chain: ")
 
-response = requests.get(f"https://api.llama.fi/overview/fees/{Chain}?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=true&dataType={Time}Fees")
-data = response.json()
+    # Time frame to observe (daily or weekly)
+    Time = input("Enter the type (daily or total): ")
 
-# Initialize an empty list to store the data
-rows = []
+    response = requests.get(
+        f"https://api.llama.fi/overview/fees/{Chain}?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=true&dataType={Time}Fees"
+    )
+    data = response.json()
 
-for protocol in data["protocols"]:
-    # Append a new row to the list for each protocol
-    row = {
-        "protocol_name": protocol["name"],
-        "total24h": protocol["total24h"],
-        "change_1d": protocol["change_1d"],
-        "change_7d": protocol["change_7d"],
-        "change_1m": protocol["change_1m"],
-    }
-    rows.append(row)
+    # Initialize an empty list to store the data
+    rows = []
 
-# Create a pandas DataFrame from the list of rows
-df = pd.DataFrame(rows)
+    for protocol in data["protocols"]:
+        # Append a new row to the list for each protocol
+        row = {
+            "protocol_name": protocol["name"],
+            "total24h": protocol["total24h"],
+            "change_1d": protocol["change_1d"],
+            "change_7d": protocol["change_7d"],
+            "change_1m": protocol["change_1m"],
+        }
+        rows.append(row)
 
-# Reorder the columns to make the protocol name the first column
-df = df[["protocol_name", "total24h", "change_1d", "change_7d", "change_1m"]]
+    # Create a pandas DataFrame from the list of rows
+    df = pd.DataFrame(rows)
 
-# Write the DataFrame to a CSV file
-df.to_csv("protocol_fees_by_chain.csv", index=False)
-print("Data written to protocol_fees_by_chain.csv")
+    # Reorder the columns to make the protocol name the first column
+    df = df[["protocol_name", "total24h", "change_1d", "change_7d", "change_1m"]]
+
+    # Write the DataFrame to a CSV file with Chain name in the filename
+    csv_filename = f"{Chain}_{Time}_fee_breakdown.csv"
+    df.to_csv(csv_filename, index=False)
+    print(f"Data written to {csv_filename}")
+
+
+if __name__ == "__main__":
+    main()
