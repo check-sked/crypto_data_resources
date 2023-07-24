1ba2c4d7df988bf6da9a890231636a4abf3e1cf7
Check <check.sked@gmail.com>
2023-07-24 12:25:34 -0400

    Updated README

--- defi_protocols_chains/chain_list.py
+++ defi_protocols_chains/chain_list.py
@@ -1,34 +1,38 @@
 import csv
 import requests
 
+
 def write_to_csv(data):
-    with open('chains_list.csv', mode='w') as csv_file:
-        fieldnames = ['name']
+    with open("chains_list.csv", mode="w") as csv_file:
+        fieldnames = ["name"]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
         for item in data:
-            writer.writerow({'name': item['name']})
+            writer.writerow({"name": item["name"]})
+
 
 def get_data():
     try:
-        url = 'https://api.llama.fi/chains'
+        url = "https://api.llama.fi/chains"
         response = requests.get(url)
         response.raise_for_status()
         chains = response.json()
         data = []
         for chain in chains:
-            data.append({'name': chain['name']})
+            data.append({"name": chain["name"]})
         return data
     except requests.exceptions.HTTPError as errh:
-        print("Http Error:",errh)
+        print("Http Error:", errh)
     except requests.exceptions.ConnectionError as errc:
-        print("Error Connecting:",errc)
+        print("Error Connecting:", errc)
     except requests.exceptions.Timeout as errt:
-        print("Timeout Error:",errt)
+        print("Timeout Error:", errt)
     except requests.exceptions.RequestException as err:
-        print("Something went wrong:",err)
+        print("Something went wrong:", err)
 
-data = get_data()
-data = sorted(data, key=lambda x: x['name'].lower())
-write_to_csv(data)
-print("CSV file 'chains_list.csv' created successfully!")
+
+if __name__ == "__main__":
+    data = get_data()
+    data = sorted(data, key=lambda x: x["name"].lower())
+    write_to_csv(data)
+    print("CSV file 'chains_list.csv' created successfully!")
--- defi_protocols_chains/protocols_list.py
+++ defi_protocols_chains/protocols_list.py
@@ -1,34 +1,40 @@
 import csv
 import requests
 
-try:
-    # Retrieve data from API
-    response = requests.get("https://api.llama.fi/protocols")
-    response.raise_for_status()
-    data = response.json()
 
-    # Sort data alphabetically by name and by chain within name
-    data = sorted(data, key=lambda x: x['name'])
-    for protocol in data:
-        protocol['chains'] = sorted(protocol['chains'])
+def main():
+    try:
+        # Retrieve data from API
+        response = requests.get("https://api.llama.fi/protocols")
+        response.raise_for_status()
+        data = response.json()
 
-    with open('protocols_list.csv', mode='w') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Name", "Chain","Category"])
-        print("data written to protocols_list.csv")
-
+        # Sort data alphabetically by name and by chain within name
+        data = sorted(data, key=lambda x: x["name"])
         for protocol in data:
-            for chain in protocol["chains"]:
-                try:
-                    writer.writerow([protocol["name"], chain, protocol["category"]])
-                except KeyError:
-                    writer.writerow([protocol["name"], chain, ""])
+            protocol["chains"] = sorted(protocol["chains"])
 
-except requests.exceptions.HTTPError as errh:
-    print ("HTTP Error:",errh)
-except requests.exceptions.ConnectionError as errc:
-    print ("Error Connecting:",errc)
-except requests.exceptions.Timeout as errt:
-    print ("Timeout Error:",errt)
-except requests.exceptions.RequestException as err:
-    print ("Something went wrong:",err)
+        with open("protocols_list.csv", mode="w") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Name", "Chain", "Category"])
+            print("Data written to protocols_list.csv")
+
+            for protocol in data:
+                for chain in protocol["chains"]:
+                    try:
+                        writer.writerow([protocol["name"], chain, protocol["category"]])
+                    except KeyError:
+                        writer.writerow([protocol["name"], chain, ""])
+
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("Something went wrong:", err)
+
+
+if __name__ == "__main__":
+    main()
