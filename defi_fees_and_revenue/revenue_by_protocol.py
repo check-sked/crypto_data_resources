5e136b85e3c138b93ffd723152fa365723593f48
Check <check.sked@gmail.com>
2023-07-24 10:51:14 -0400

    Updated to if __name__ == __main__ and cleaned up formatting

--- defi_fees_and_revenue/revenue_by_protocol.py
+++ defi_fees_and_revenue/revenue_by_protocol.py
@@ -3,40 +3,60 @@ import requests
 from datetime import datetime
 from collections import defaultdict
 
-# Input protocol to observe. Reference protocols_list_fees.csv for available protocols.
-Protocol = "aave"
 
-# Input type (daily or total - no space and no caps)
-Type = "daily"
+def main():
+    # Input protocol to observe. Reference protocols_list_fees.csv for available protocols.
+    Protocol = input("Enter the protocol: ")
 
-response = requests.get(f"https://api.llama.fi/summary/fees/{Protocol}?dataType={Type}Revenue")
-data = response.json()
+    # Input type (daily or total - no space and no caps)
+    Type = input("Enter the type (daily or total): ")
 
-all_chains = set()
-all_versions = set()
-breakdown = defaultdict(lambda: defaultdict(int))
-for day in data['totalDataChartBreakdown']:
-    timestamp = day[0]
-    date = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
-    for chain, fees_by_version in day[1].items():
-        all_chains.add(chain)
-        for version, fees in fees_by_version.items():
-            all_versions.add(version)
-            breakdown[date][chain+'_'+version] += fees
+    response = requests.get(
+        f"https://api.llama.fi/summary/fees/{Protocol}?dataType={Type}Revenue"
+    )
+    data = response.json()
 
-with open('protocol_revenue_by_chain.csv', mode='w', newline='') as csv_file:
-    fieldnames = ['date'] + sorted([chain+'_'+version for chain in all_chains for version in all_versions]) + ['total_fees']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    print("data written to protocol_revenue_by_chain.csv")
+    all_chains = set()
+    all_versions = set()
+    breakdown = defaultdict(lambda: defaultdict(int))
+    for day in data["totalDataChartBreakdown"]:
+        timestamp = day[0]
+        date = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d")
+        for chain, fees_by_version in day[1].items():
+            all_chains.add(chain)
+            for version, fees in fees_by_version.items():
+                all_versions.add(version)
+                breakdown[date][chain + "_" + version] += fees
 
-    for date, fees_by_chain in breakdown.items():
-        row = {'date': date}
-        total_fees = 0
-        for chain in all_chains:
-            for version in all_versions:
-                key = chain+'_'+version
-                row[key] = fees_by_chain.get(key, 0)
-                total_fees += fees_by_chain.get(key, 0)
-        row['total_fees'] = total_fees
-        writer.writerow(row)
+    csv_filename = f"{Protocol}_{Type}_revenue.csv"  # Filename with protocol and type
+
+    with open(csv_filename, mode="w", newline="") as csv_file:
+        fieldnames = (
+            ["date"]
+            + sorted(
+                [
+                    chain + "_" + version
+                    for chain in all_chains
+                    for version in all_versions
+                ]
+            )
+            + ["total_fees"]
+        )
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        print(f"Data written to {csv_filename}")
+
+        for date, fees_by_chain in breakdown.items():
+            row = {"date": date}
+            total_fees = 0
+            for chain in all_chains:
+                for version in all_versions:
+                    key = chain + "_" + version
+                    row[key] = fees_by_chain.get(key, 0)
+                    total_fees += fees_by_chain.get(key, 0)
+            row["total_fees"] = total_fees
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    main()
