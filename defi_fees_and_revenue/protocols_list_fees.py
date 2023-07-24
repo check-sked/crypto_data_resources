c6f1a3a291d68ce1d20903f470bff2cf49d95463
Check <check.sked@gmail.com>
2023-07-24 10:45:06 -0400

    Updated to if __name__ == __main__

--- defi_fees_and_revenue/protocols_list_fees.py
+++ defi_fees_and_revenue/protocols_list_fees.py
@@ -1,16 +1,24 @@
 import csv
 import requests
 
-url = 'https://api.llama.fi/overview/fees?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=true&dataType=dailyFees'
 
-response = requests.get(url)
-data = response.json()
+def main():
+    url = "https://api.llama.fi/overview/fees?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=true&dataType=dailyFees"
 
-with open('protocols_list_fees.csv', mode='w', newline='') as csv_file:
-    fieldnames = ['name', 'category']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    print("data written to protocols_list_fees.csv")
+    response = requests.get(url)
+    data = response.json()
 
-    for protocol in data['protocols']:
-        writer.writerow({'name': protocol['name'], 'category': protocol['category']})
+    with open("protocols_list_fees.csv", mode="w", newline="") as csv_file:
+        fieldnames = ["name", "category"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        print("Data written to protocols_list_fees.csv")
+
+        for protocol in data["protocols"]:
+            writer.writerow(
+                {"name": protocol["name"], "category": protocol["category"]}
+            )
+
+
+if __name__ == "__main__":
+    main()
