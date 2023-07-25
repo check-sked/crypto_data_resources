8c24483bbe57bed01bfe1d2cb8e74c5efc6f55e2
Check <check.sked@gmail.com>
2023-07-25 15:20:03 -0400

    Updated CSV file name and added print messages for file completion

--- defi_tvl/protocol_tvl.py
+++ defi_tvl/protocol_tvl.py
@@ -2,35 +2,44 @@ import csv
 import datetime
 import requests
 
-# Set the API end point URL
-protocol = "aave"
 
-# Make the API request
-response = requests.get(f"https://api.llama.fi/protocol/{protocol}")
+def main():
+    # Set the protocol
+    protocol = input("Enter the protocol name: ")
 
-# Check for successful request
-if response.status_code == 200:
-    # Get the data from the response
-    data = response.json()
+    # Make the API request
+    response = requests.get(f"https://api.llama.fi/protocol/{protocol}")
 
-    # Get the TVL data
-    tvl_data = data["tvl"]
+    # Check for successful request
+    if response.status_code == 200:
+        # Get the data from the response
+        data = response.json()
 
-    # Open a CSV file for writing
-    with open("tvl_data.csv", "w", newline="") as csvfile:
-        # Create a CSV writer
-        writer = csv.writer(csvfile)
+        # Get the TVL data
+        tvl_data = data["tvl"]
 
-        # Write the header row to the CSV file
-        writer.writerow(["Date", "TVL"])
+        # Open a CSV file for writing
+        with open(f"{protocol}_TVL.csv", "w", newline="") as csvfile:
+            # Create a CSV writer
+            writer = csv.writer(csvfile)
 
-        # Write the TVL data to the CSV file
-        for datapoint in tvl_data:
-            # Convert the date value to a human-readable string
-            date_string = str(datapoint["date"])
-            date = datetime.datetime.fromtimestamp(int(date_string)).isoformat()
-            tvl = datapoint["totalLiquidityUSD"]
-            writer.writerow([date, tvl])
-else:
-    # Print an error message
-    print("Failed to retrieve data")
+            # Write the header row to the CSV file
+            writer.writerow(["Date", "TVL"])
+
+            # Write the TVL data to the CSV file
+            for datapoint in tvl_data:
+                # Convert the date value to a human-readable string
+                date_string = str(datapoint["date"])
+                date = datetime.datetime.fromtimestamp(int(date_string)).isoformat()
+                tvl = datapoint["totalLiquidityUSD"]
+                writer.writerow([date, tvl])
+
+        # Print a success message
+        print(f"Data successfully written to {protocol}_TVL.csv!")
+    else:
+        # Print an error message
+        print("Failed to retrieve data")
+
+
+if __name__ == "__main__":
+    main()
