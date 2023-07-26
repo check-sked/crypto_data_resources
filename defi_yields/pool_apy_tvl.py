3496bfdf39b6590b32616ff57dc1498ea8495964
Check <check.sked@gmail.com>
2023-07-26 12:21:10 -0400

    Put in if __name__ == __main__ format and added input prompts where needed

--- defi_yields/pool_apy_tvl.py
+++ defi_yields/pool_apy_tvl.py
@@ -2,24 +2,32 @@ import csv
 import requests
 import datetime
 
-# Input Pool ID (Pool IDs can be found in the Pool Glossary)
-Pool_ID = "4399e512-4ce8-44cd-97af-13fd893b9f71"
 
-response = requests.get(f"https://yields.llama.fi/chart/{Pool_ID}")
-data = response.json()
+def get_pool_data():
+    Pool_ID = input("Enter the Pool ID: ")
+    print("The script is running. Press CTRL + C to kill operation at any time.")
 
-# Open the file in write mode
-with open('pool_tvl_apy.csv', mode='w', newline='') as file:
-    # Create a CSV writer
-    writer = csv.writer(file)
+    response = requests.get(f"https://yields.llama.fi/chart/{Pool_ID}")
+    data = response.json()
 
-    # Write the headers to the CSV file
-    writer.writerow(["timestamp", "tvlUSD", "apy"])
+    # Open the file in write mode
+    with open("pool_tvl_apy.csv", mode="w", newline="") as file:
+        # Create a CSV writer
+        writer = csv.writer(file)
 
-    # Write the data to the CSV file
-    for item in data["data"]:
-        timestamp = datetime.datetime.strptime(item['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%m/%d/%Y')
-        tvlUSD = item['tvlUsd']
-        apy = round(item['apy']/100, 5)
-        writer.writerow([timestamp, tvlUSD, apy])
-    print("Data written to pool_tvl_apy.csv")
+        # Write the headers to the CSV file
+        writer.writerow(["timestamp", "tvlUSD", "apy"])
+
+        # Write the data to the CSV file
+        for item in data["data"]:
+            timestamp = datetime.datetime.strptime(
+                item["timestamp"], "%Y-%m-%dT%H:%M:%S.%fZ"
+            ).strftime("%m/%d/%Y")
+            tvlUSD = item["tvlUsd"]
+            apy = round(item["apy"] / 100, 5)
+            writer.writerow([timestamp, tvlUSD, apy])
+        print("Data written to pool_tvl_apy.csv")
+
+
+if __name__ == "__main__":
+    get_pool_data()
--- defi_yields/pools_glossary.py
+++ defi_yields/pools_glossary.py
@@ -1,19 +1,27 @@
 import csv
 import requests
 
-response = requests.get('https://yields.llama.fi/pools')
-data = response.json()
 
-data = sorted(data["data"], key=lambda x: (x["chain"], x["project"]))
+def get_pools_glossary():
+    print("The script is running. Press CTRL + C to kill operation at any time.")
 
-with open('pools_glossary.csv', mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Chain', 'Project', 'Symbol', 'Pool_ID'])
-    for item in data:
-        project = item['project']
-        symbol = item['symbol']
-        pool = item['pool']
-        chain = item['chain']
-        writer.writerow([chain, project, symbol, pool])
+    response = requests.get("https://yields.llama.fi/pools")
+    data = response.json()
 
-print("Data written to pools_glossary.csv")
+    data = sorted(data["data"], key=lambda x: (x["chain"], x["project"]))
+
+    with open("pools_glossary.csv", mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Chain", "Project", "Symbol", "Pool_ID"])
+        for item in data:
+            project = item["project"]
+            symbol = item["symbol"]
+            pool = item["pool"]
+            chain = item["chain"]
+            writer.writerow([chain, project, symbol, pool])
+
+    print("Data written to pools_glossary.csv")
+
+
+if __name__ == "__main__":
+    get_pools_glossary()
