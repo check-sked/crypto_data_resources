ca6442b9d2908e8f7c6aaaa7129ef2dcd7c51d72
Check <check.sked@gmail.com>
2023-07-26 11:45:25 -0400

    Updated to if __name__ == __main__ format and enhanced in prompt messaging and inputs

--- defi_tvl/protocol_tvl_by_token.py
+++ defi_tvl/protocol_tvl_by_token.py
@@ -2,42 +2,50 @@ import pandas as pd
 import requests
 from datetime import datetime
 
-Protocol = "binance-cex"
 
-try:
-    response = requests.get(f"https://api.llama.fi/protocol/{Protocol}")
-    response.raise_for_status()
-    data = response.json()
-except requests.exceptions.RequestException as e:
-    print("An error occurred while making the API request:", e)
-    raise SystemExit(1)
+def get_protocol_data():
+    Protocol = input("Enter the protocol: ")
+    print("The script is running. Press CTRL + C to kill operation at any time.")
 
-# Create an empty list to store the data in
-output_data = []
+    try:
+        response = requests.get(f"https://api.llama.fi/protocol/{Protocol}")
+        response.raise_for_status()
+        data = response.json()
+    except requests.exceptions.RequestException as e:
+        print("An error occurred while making the API request:", e)
+        raise SystemExit(1)
 
-# Iterate through the list of tokens in the response
-for token_data in data["tokens"]:
-    date = datetime.fromtimestamp(token_data["date"]).strftime("%Y-%m-%d")
-    tokens = token_data["tokens"]
-    for token, tvl in tokens.items():
-        output_data.append({"date": date, "token": token, "tvl": tvl})
+    # Create an empty list to store the data in
+    output_data = []
 
-# Convert the list of dictionaries to a DataFrame
-output_df = pd.DataFrame(output_data)
+    # Iterate through the list of tokens in the response
+    for token_data in data["tokens"]:
+        date = datetime.fromtimestamp(token_data["date"]).strftime("%Y-%m-%d")
+        tokens = token_data["tokens"]
+        for token, tvl in tokens.items():
+            output_data.append({"date": date, "token": token, "tvl": tvl})
 
-# group by date and token
-grouped_df = output_df.groupby(["date", "token"], as_index=False).sum()
+    # Convert the list of dictionaries to a DataFrame
+    output_df = pd.DataFrame(output_data)
 
-# unstack the dataframe
-pivot_df = grouped_df.pivot(index="date", columns="token", values="tvl")
+    # group by date and token
+    grouped_df = output_df.groupby(["date", "token"], as_index=False).sum()
 
-# Add the date column
-pivot_df.insert(0, "date", pivot_df.index)
+    # unstack the dataframe
+    pivot_df = grouped_df.pivot(index="date", columns="token", values="tvl")
 
-# Save the DataFrame to a CSV file named output.csv
-try:
-    pivot_df.to_csv("protocol_tvl_by_token.csv", index=False)
-    print("Data written to protocol_tvl_by_token.csv")
-except Exception as e:
-    print("An error occurred while writing the CSV file:", e)
-    raise SystemExit(1)
+    # Add the date column
+    pivot_df.insert(0, "date", pivot_df.index)
+
+    # Save the DataFrame to a CSV file named output.csv
+    try:
+        filename = f"{Protocol}_TVL_by_Token.csv"
+        pivot_df.to_csv(filename, index=False)
+        print(f"Data successfully written to {filename}!")
+    except Exception as e:
+        print("An error occurred while writing the CSV file:", e)
+        raise SystemExit(1)
+
+
+if __name__ == "__main__":
+    get_protocol_data()
