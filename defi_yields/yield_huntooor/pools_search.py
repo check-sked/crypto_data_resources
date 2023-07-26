d03fbcfd01617422206436a9c90b689f49405e42
Check <check.sked@gmail.com>
2023-07-26 16:09:16 -0400

    Updated to if __name__ == __main__ and sorted by pool APY

--- defi_yields/yield_huntooor/pools_search.py
+++ defi_yields/yield_huntooor/pools_search.py
@@ -2,33 +2,48 @@ import requests
 import json
 import csv
 
-url = "https://yields.llama.fi/pools"
-try:
-    # Make API request and get response
-    response = requests.get(url)
-    # Check if the response is successful
-    response.raise_for_status()
-    if response.headers['Content-Type'] != 'application/json':
-        print("Unexpected Content-Type: ", response.headers['Content-Type'])
-        raise ValueError("Unexpected Content-Type")
-    data = json.loads(response.text)
 
-    # Sorting the data by APY in descending order
-    data = sorted(data["data"], key=lambda x: x["apy"], reverse=True)
+def get_all_pools_apy_sorted():
+    url = "https://yields.llama.fi/pools"
+    print("The script is running. Press CTRL + C to kill operation at any time.")
+    try:
+        # Make API request and get response
+        response = requests.get(url)
+        # Check if the response is successful
+        response.raise_for_status()
+        if response.headers["Content-Type"] != "application/json":
+            print("Unexpected Content-Type: ", response.headers["Content-Type"])
+            raise ValueError("Unexpected Content-Type")
+        data = json.loads(response.text)
 
-    # Open a CSV file for writing
-    with open("all_pools_by_apy.csv", "w", newline="") as csvfile:
-        # Create a CSV writer
-        writer = csv.writer(csvfile)
+        # Sorting the data by APY in descending order
+        data = sorted(data["data"], key=lambda x: x["apy"], reverse=True)
 
-        # Write the header row
-        writer.writerow(["chain", "project", "symbol", "apy", "pool"])
+        # Open a CSV file for writing
+        with open("all_pools_apy_sorted.csv", "w", newline="") as csvfile:
+            # Create a CSV writer
+            writer = csv.writer(csvfile)
 
-        # Write the data rows
-        for pool in data:
-            writer.writerow([pool["chain"], pool["project"], pool["symbol"], pool["apy"], pool["pool"]])
-    print("data written to all_pools_by_apy.csv")
-except requests.exceptions.HTTPError as err:
-    print(f"HTTP Error Occured: {err}")
-except requests.exceptions.RequestException as err:
-    print(f"Something went wrong: {err}")
+            # Write the header row
+            writer.writerow(["chain", "project", "symbol", "apy", "pool"])
+
+            # Write the data rows
+            for pool in data:
+                writer.writerow(
+                    [
+                        pool["chain"],
+                        pool["project"],
+                        pool["symbol"],
+                        pool["apy"],
+                        pool["pool"],
+                    ]
+                )
+        print("data written to all_pools_by_apy.csv")
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP Error Occured: {err}")
+    except requests.exceptions.RequestException as err:
+        print(f"Something went wrong: {err}")
+
+
+if __name__ == "__main__":
+    get_all_pools_apy_sorted()
