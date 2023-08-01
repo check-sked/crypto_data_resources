a656e6f477b014517223dae066ea0c3027221fee
Check <check.sked@gmail.com>
2023-08-01 19:31:40 -0400

    Put in if __name__ == __main__ and improved prompting

--- staking/staking_by_protocol.py
+++ staking/staking_by_protocol.py
@@ -2,35 +2,44 @@ import csv
 import datetime
 import requests
 
-# Set the API end point URL
-protocol = input("Enter a protocol: ")
 
-# Make the API request
-response = requests.get(f"https://api.llama.fi/protocol/{protocol}")
+def main():
+    # Set the API end point URL
+    protocol = input("Enter a protocol: ")
+    print("The script is running. Press CTRL + C to kill the operation at any time.")
 
-# Check for successful request
-if response.status_code == 200:
-  # Get the data from the response
-  data = response.json()
+    # Make the API request
+    response = requests.get(f"https://api.llama.fi/protocol/{protocol}")
 
-  # Get the TVL data
-  staking_data = data["chainTvls"]["staking"]["tvl"]
+    # Check for successful request
+    if response.status_code == 200:
+        # Get the data from the response
+        data = response.json()
 
-  # Open a CSV file for writing
-  with open(f"{protocol}_staking.csv", "w", newline="") as csvfile:
-    # Create a CSV writer
-    writer = csv.writer(csvfile)
+        # Get the TVL data
+        staking_data = data["chainTvls"]["staking"]["tvl"]
 
-    # Write the header row to the CSV file
-    writer.writerow(["Date", "Staking"])
-    print(f"Data written to {protocol}_staking.csv!")
+        # Open a CSV file for writing
+        with open(f"{protocol}_staking.csv", "w", newline="") as csvfile:
+            # Create a CSV writer
+            writer = csv.writer(csvfile)
 
-    # Write the TVL data to the CSV file
-    for datapoint in staking_data:
-      # Convert the date value to a human-readable string
-      date = datetime.datetime.fromtimestamp(int(datapoint["date"])).strftime('%m/%d/%Y')
-      tvl = datapoint["totalLiquidityUSD"]
-      writer.writerow([date, tvl])
-else:
-  # Print an error message
-  print("Failed to retrieve data")
+            # Write the header row to the CSV file
+            writer.writerow(["Date", "Staking"])
+            print(f"Data written to {protocol}_staking.csv!")
+
+            # Write the TVL data to the CSV file
+            for datapoint in staking_data:
+                # Convert the date value to a human-readable string
+                date = datetime.datetime.fromtimestamp(int(datapoint["date"])).strftime(
+                    "%m/%d/%Y"
+                )
+                tvl = datapoint["totalLiquidityUSD"]
+                writer.writerow([date, tvl])
+    else:
+        # Print an error message
+        print("Failed to retrieve data")
+
+
+if __name__ == "__main__":
+    main()
